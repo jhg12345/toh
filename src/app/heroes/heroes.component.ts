fb0575043c9914cacc7f4b10deea4b15c002859a
kosmo24 <bl085@naver.com>
2018-04-07 17:50:52 +0900

    6장 서비스

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
 import {HEROES} from '../HEROES';
+import {Hero} from '../hero';
+import {HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,16 +14,26 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };*/
 
-  //hero = new Hero();
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
-  constructor() {
+  // 2) 서비스를 생성자로 주입
+  constructor(private heroService: HeroService) {
     /*this.hero.id = 1;
     this.hero.name = 'test';*/
+
   }
 
   ngOnInit() {
+    // 서비스 호출
+    // 1) 잘못된 방법 : 의존성이 생김. 컴포넌트가 서비스를 제어
+    // const heroService = new HeroService();
+    // this.heroes = heroService.getHeroes();
+    // 2) 잘된 방법
+    this.heroService.getHeroes()
+      .subscribe(data => {
+        this.heroes = data;
+      });
   }
 
   onSelect(hero: Hero): void {
